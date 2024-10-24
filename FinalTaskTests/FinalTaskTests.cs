88f490c280450691a3f77e82c2d5831dc7061cce
rafafa81 <39038749+rafafa81@users.noreply.github.com>
2024-10-23 22:44:29 -0600

    finaltask_implemented_null_validations_for_elements

--- FinalTaskTests/FinalTaskTests.cs
+++ FinalTaskTests/FinalTaskTests.cs
@@ -19,16 +19,19 @@ namespace FinalTask.Tests
     [TestClass()]
     public class FinalTaskTests
     {
-        public IWebDriver driver;
-        public readonly string usernameLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input";
-        public readonly string passwordLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input";
-        public readonly string loginButtonLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/input";
-        public readonly string errorCardLocator = "/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3";
-        public readonly string dashboardNameLocator = "/html/body/div/div/div/div[1]/div[1]/div[2]/div";
+        public IWebDriver? driver;
+        private readonly string usernameLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input";
+        private readonly string passwordLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input";
+        private readonly string loginButtonLocatorPath = "/html/body/div/div/div[2]/div[1]/div/div/form/input";
+        private readonly string errorCardLocator = "/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3";
+        private readonly string dashboardNameLocator = "/html/body/div/div/div/div[1]/div[1]/div[2]/div";
                  
-        WebDriverWait wait;
-        int SECONDS_TO_WAIT = 5;
-
+        private WebDriverWait? wait;
+        private readonly int SECONDS_TO_WAIT = 5;
+        private IWebElement? inputUsername;
+        private IWebElement? inputPassword;
+        private IWebElement? loginButton;
+        private string? errorMessage;
 
         [TestInitialize]
         public void Init()
@@ -45,6 +48,10 @@ namespace FinalTask.Tests
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Navigate().GoToUrl(urlPagina);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(SECONDS_TO_WAIT));
+            inputUsername = driver.FindElement(By.XPath(usernameLocatorPath));
+            inputPassword = driver.FindElement(By.XPath(passwordLocatorPath));
+            loginButton = driver.FindElement(By.XPath(loginButtonLocatorPath));
+            errorMessage = string.Empty;
         }
 
         [TestMethod("UC-1")]
@@ -53,30 +60,26 @@ namespace FinalTask.Tests
         {
 
             Log.Debug("Initiating UC-1 test");
-            Actions builder = new Actions(driver);
-            var inputUsername = driver.FindElement(By.XPath(usernameLocatorPath));
-            var inputPassword = driver.FindElement(By.XPath(passwordLocatorPath));
-            inputUsername.Clear();
-            inputPassword.Clear();
-            inputUsername.SendKeys("Random");
-            inputPassword.SendKeys("Random");
-            Log.Debug("User and password filled");
-            inputUsername.Clear();
-            inputPassword.Clear();
-            inputUsername.SendKeys(" ");
-            inputPassword.Clear();
-            inputPassword.SendKeys(" ");
-            inputUsername.SendKeys(Keys.Backspace);
-            inputPassword.SendKeys(Keys.Backspace);
-            Log.Debug("User and password deleted");
-        
-            var loginButton = driver.FindElement(By.XPath(loginButtonLocatorPath));
-            
-            loginButton.Click();
-            Log.Debug("Login button clicked");
-            var errorMessage = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(errorCardLocator))).Text;
+            if (inputUsername is not null && inputPassword is not null && loginButton is not null && wait is not null)
+            {
+                inputUsername.Clear();
+                inputPassword.Clear();
+                inputUsername.SendKeys("Random");
+                inputPassword.SendKeys("Random");
+                Log.Debug("User and password filled");
+                inputUsername.Clear();
+                inputPassword.Clear();
+                inputUsername.SendKeys(" ");
+                inputPassword.Clear();
+                inputPassword.SendKeys(" ");
+                inputUsername.SendKeys(Keys.Backspace);
+                inputPassword.SendKeys(Keys.Backspace);
+                Log.Debug("User and password deleted");
+                loginButton.Click();
+                Log.Debug("Login button clicked");
+                errorMessage = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(errorCardLocator))).Text;
+            }
            
-            
             Assert.AreEqual("Epic sadface: Username is required", errorMessage);
         }
 
@@ -86,26 +89,22 @@ namespace FinalTask.Tests
         {
             
             Log.Debug("Initiating UC-2 test");
-
-            Actions builder = new Actions(driver);
-            var inputUsername = driver.FindElement(By.XPath(usernameLocatorPath));
-            var inputPassword = driver.FindElement(By.XPath(passwordLocatorPath));
-            inputUsername.Clear();
-            inputPassword.Clear();
-            inputUsername.SendKeys("Random");
-            inputPassword.SendKeys("Random");
-            Log.Debug("User and password filled");
-            inputPassword.Clear();
-            inputPassword.Clear();
-            inputPassword.SendKeys(" ");
-            inputPassword.SendKeys(Keys.Backspace);
-            Log.Debug("Password deleted");
-            var loginButton = driver.FindElement(By.XPath(loginButtonLocatorPath));
-           
-            loginButton.Click();
-            Log.Debug("Login button clicked");
-            var errorMessage = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(errorCardLocator))).Text;
-            
+            if (inputUsername is not null && inputPassword is not null && loginButton is not null && wait is not null)
+            {
+                inputUsername.Clear();
+                inputPassword.Clear();
+                inputUsername.SendKeys("Random");
+                inputPassword.SendKeys("Random");
+                Log.Debug("User and password filled");
+                inputPassword.Clear();
+                inputPassword.Clear();
+                inputPassword.SendKeys(" ");
+                inputPassword.SendKeys(Keys.Backspace);
+                Log.Debug("Password deleted");
+                loginButton.Click();
+                Log.Debug("Login button clicked");
+                errorMessage = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(errorCardLocator))).Text;
+            }
             
             Assert.AreEqual("Epic sadface: Password is required", errorMessage);
         }
@@ -116,13 +115,17 @@ namespace FinalTask.Tests
         {
             
             Log.Information("Initiating UC-3 test");
+            var dashboardNameText = string.Empty;
 
-            driver.FindElement(By.XPath(usernameLocatorPath)).SendKeys(username);
-            driver.FindElement(By.XPath(passwordLocatorPath)).SendKeys(password);
-            Log.Debug("User and password filled");
-            driver.FindElement(By.XPath(loginButtonLocatorPath)).Click();
-            Log.Debug("Login button clicked");
-            var dashboardNameText = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(dashboardNameLocator))).Text;
+            if (inputUsername is not null && inputPassword is not null && loginButton is not null && wait is not null)
+            {
+                inputUsername.SendKeys(username);
+                inputPassword.SendKeys(password);
+                Log.Debug("User and password filled");
+                loginButton.Click();
+                Log.Debug("Login button clicked");
+                dashboardNameText = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(dashboardNameLocator))).Text;
+            }
             
             Assert.AreEqual("Swag Labs", dashboardNameText);
         }
@@ -130,7 +133,10 @@ namespace FinalTask.Tests
         [TestCleanup]
         public void Cleanup()
         {
-            driver.Close(); 
+            if (driver is not null)
+            {
+                driver.Close(); 
+            }
         }
     }
 }
--- FinalTaskTests/obj/Debug/net8.0/FinalTaskTests.AssemblyInfo.cs
+++ FinalTaskTests/obj/Debug/net8.0/FinalTaskTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalTaskTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eafcab7827ad5a497e9ddbf97c05f152efa46fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e154dc1fd151495205b777905e3c1c3a25d07d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalTaskTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalTaskTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
